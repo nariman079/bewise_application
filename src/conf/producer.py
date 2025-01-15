098e6832626b1cefe161bcad76eb95be15b3a5a0
nariman079 <nariman079i@mail.ru>
2025-01-15 16:43:47 +0300

    fix producer work

--- src/conf/producer.py
+++ src/conf/producer.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 
 from aiokafka import AIOKafkaProducer
@@ -5,9 +6,13 @@ from aiokafka import AIOKafkaProducer
 from src.conf import KAFKA_BROKERCONNECT as kafka_connection_url
 
 loop = asyncio.get_event_loop()
-producer = AIOKafkaProducer(bootstrap_servers=kafka_connection_url, loop=loop)
+producer = AIOKafkaProducer(
+    bootstrap_servers=kafka_connection_url, 
+    loop=loop,
+    value_serializer=lambda v: json.dumps(v).encode('utf-8') 
+)
 
 
-async def send_message(topic_name: str, message: str) -> None:
+async def send_message(topic_name: str, value: dict) -> None:
     """Отправка сообщения в топик Kafka"""
-    await producer.send(topic_name, message)
+    await producer.send(topic_name, value)
